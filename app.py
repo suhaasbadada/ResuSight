25a85b6a1514eb1c706472fd9b63608e34e77aa0
suhaasbadada <suhaasbadada@gmail.com>
2023-10-15 11:40:33 +0530

    added swagger gui

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from gpt.langchain_models import jd_questions
 from strawberryGQL.queries import Query
 from strawberryGQL.mutations import Mutation
 from strawberry.schema.config import StrawberryConfig
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app=Flask(__name__)
 
@@ -20,6 +21,20 @@ MONGO_ATLAS_DB_NAME=os.getenv('MONGO_ATLAS_DB_NAME')
 MONGO_URI=f"mongodb+srv://{MONGO_ATLAS_USERNAME}:{MONGO_ATLAS_PASSWORD}@{MONGO_ATLAS_CLUSTER_ADDR}.mongodb.net/{MONGO_ATLAS_DB_NAME}?retryWrites=true&w=majority"
 app.config['MONGO_URI']=MONGO_URI
 mongo.init_app(app)
+
+# Swagger Config
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'ResuSight API Docs'
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 
 schema=strawberry.Schema(query=Query, mutation=Mutation, config=StrawberryConfig(auto_camel_case=False))
