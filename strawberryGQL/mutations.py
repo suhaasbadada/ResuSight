806c1daab4636d1f12c57f5a6109099f8404b565
suhaasbadada <suhaasbadada@gmail.com>
2023-10-23 19:58:55 +0530

    Revert "made login into a query & used it for /login"
    This reverts commit 1a8285abcf5705a23c9c11a3a33a39cc4874c4dc.

--- strawberryGQL/mutations.py
+++ strawberryGQL/mutations.py
@@ -4,7 +4,7 @@ import strawberry
 from strawberry.types import Info
 from mongoDatabase.db import mongo
 from strawberryGQL.gql_schema import AuthResponse, Resume, UploadResponse, User, UserInput
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @strawberry.type
 class Mutation:
@@ -27,6 +27,17 @@ class Mutation:
         }
         mongo.db.user_collection.insert_one(new_user)
         return AuthResponse(message='Registration Successful. Proceed to log in')
+    
+    @strawberry.mutation
+    def login_user(info: Info,username: str, password: str) ->AuthResponse:
+        user=mongo.db.user_collection.find_one({'username':username})
+
+        if user and check_password_hash(user['password'],password):
+            user_info = {'username': user['username'], 'email': user['email']}
+            token = jwt.encode(user_info, "secret", algorithm="HS256")
+            return AuthResponse(message='Login Successful',user=User(username=user['username'], email=user['email']), token=token)
+        
+        return AuthResponse(message='Invalid Credentials', user=None, token=None)
 
     @strawberry.mutation
     def upload_or_update_resume(info: Info, resume:Resume) ->UploadResponse:
